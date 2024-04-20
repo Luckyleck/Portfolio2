1c79f8427611355f4e1158a6a0846fc15a32eb1f
Alex <luckyleck29@gmail.com>
2024-04-20 09:27:21 -0400

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,13 +7,13 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Alex's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Alex Lecky Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "http://alexlecky.com/",
   },
 };
 
