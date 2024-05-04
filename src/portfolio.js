1210598ffa595500c47e0a427e3c3bfd99dfaabf
Alex L <118956261+Luckyleck@users.noreply.github.com>
2024-05-03 21:24:02 -0400

    Updating degree info

--- src/portfolio.js
+++ src/portfolio.js
@@ -324,9 +324,9 @@ const degrees = {
       alt_name: "App Academy",
       duration: "Dec 2022 - April 2023",
       descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ Completed in-person Full Stack development course in New York City",
+        "⚡ Focused learning with Ruby on Rails, React, Redux, and MERN Stack",
+        "⚡ Built 3 Portfolio projects, all of which can be found on the projects page",
       ],
       website_link: "https://www.appacademy.io/",
     },
@@ -337,9 +337,9 @@ const degrees = {
       alt_name: "cambridge uni logo",
       duration: "Sep 2018",
       descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+        "⚡ Completed intensive training in English language teaching methodologies, including lesson planning, classroom management, and language assessment techniques.",
+        "⚡ Developed practical skills through supervised teaching practice with adult learners from diverse linguistic and cultural backgrounds.",
+        "⚡ Received guidance and feedback from experienced tutors to enhance teaching effectiveness and adaptability in various teaching contexts.",
       ],
       website_link:
         "https://www.cambridgeenglish.org/teaching-english/teaching-qualifications/celta/",
